b2e6a75de9719e954182a2e68a9fbd6912660724
saddam-dev-9t5 <saddam.dev.9t5@gmail.com>
2023-12-30 16:57:06 +0530

    Resolving issue

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -15,87 +15,87 @@ public class MovieRepository {
 
     public String addMovie(Movie movie) {
         String name = movie.getName();
-        movieList.put(name, movie);
+        this.movieList.put(name, movie);
         return "Movie added successfully";
     }
 
     public String addDirector(Director director) {
         String name = director.getName();
-        directorList.put(name, director);
+        this.directorList.put(name, director);
         return "Director added successfully";
     }
 
     public String addMovieDirectorPair(String movie, String director) {
-        if(!(movieList.containsKey(movie) && directorList.containsKey(director))) {
+        if(!(this.movieList.containsKey(movie) && this.directorList.containsKey(director))) {
             return "movie OR director is not available in DB";
         }
         List<String> movies = new ArrayList<>();
         boolean isNewMovieAdded = true;
-        if(movieDirectorPairList.containsKey(director)) {
-            movies = movieDirectorPairList.get(director);
+        if(this.movieDirectorPairList.containsKey(director)) {
+            movies = this.movieDirectorPairList.get(director);
             if(movies.remove(movie)) isNewMovieAdded = false;
         }
         movies.add(movie);
-        movieDirectorPairList.put(director, movies);
+        this.movieDirectorPairList.put(director, movies);
 
         // Increment director's number of movies
         if(isNewMovieAdded) {
-            Director directorInfo = directorList.get(director);
+            Director directorInfo = this.directorList.get(director);
             int numberOfMovie = directorInfo.getNumberOfMovies();
             directorInfo.setNumberOfMovies(numberOfMovie + 1);
-            directorList.put(director, directorInfo);
+            this.directorList.put(director, directorInfo);
         }
         return "Movie director pair added successfully";
     }
 
     public Movie getMovieByName(String name) {
         Movie movie = new Movie();
-        if(movieList.containsKey(name)) {
-            movie = movieList.get(name);
+        if(this.movieList.containsKey(name)) {
+            movie = this.movieList.get(name);
         }
         return movie;
     }
 
     public Director getDirectorByName(String name) {
         Director director = new Director();
-        if(directorList.containsKey(name)) {
-            director = directorList.get(name);
+        if(this.directorList.containsKey(name)) {
+            director = this.directorList.get(name);
         }
         return director;
     }
 
     public List<String> getMoviesByDirectorName(String director) {
         List<String> movies = new ArrayList<>();
-        if(movieDirectorPairList.containsKey(director)) {
-            movies = movieDirectorPairList.get(director);
+        if(this.movieDirectorPairList.containsKey(director)) {
+            movies = this.movieDirectorPairList.get(director);
         }
         return movies;
     }
 
     public List<String> getAllMovies() {
         List<String> allMovies = new ArrayList<>();
-        if(!movieList.isEmpty()) {
-            allMovies.addAll(movieList.keySet());
+        if(!this.movieList.isEmpty()) {
+            allMovies.addAll(this.movieList.keySet());
         }
         return allMovies;
     }
 
     public String deleteDirectorByName(String director) {
-        directorList.remove(director);
-        if(movieDirectorPairList.containsKey(director)) {
-            List<String> directorMovies = movieDirectorPairList.get(director);
+        this.directorList.remove(director);
+        if(this.movieDirectorPairList.containsKey(director)) {
+            List<String> directorMovies = this.movieDirectorPairList.get(director);
             for (String directorMovie: directorMovies) {
-                movieList.remove(directorMovie);
+                this.movieList.remove(directorMovie);
             }
-            movieDirectorPairList.remove(director);
+            this.movieDirectorPairList.remove(director);
         }
         return "Director and their movies has been removed from DB";
     }
 
     public String deleteAllDirectors() {
-        directorList.clear();
-        movieList.clear();
-        movieDirectorPairList.clear();
+        this.directorList.clear();
+        this.movieList.clear();
+        this.movieDirectorPairList.clear();
         return "All directors and movies has been deleted";
     }
 
